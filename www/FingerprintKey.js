d19e411e505e2d7c7350a8b6e41fdde3e4ec1ed4
Won-Beom Kim <shepelt@blocko.io>
2016-10-18 10:08:47 +0900

    added common iOS/Android js plugin hook

--- www/FingerprintKey.js
+++ www/FingerprintKey.js
@@ -4,29 +4,88 @@ function createKeyFromHexSeed(seed) {
 
 function FingerprintKey() {}
 
-FingerprintKey.prototype.initKey = function(params, successCallback, errorCallback) {
-    cordova.exec(
-        function(res) {
-            if (res.status == "ok") {
-                res.key = createKeyFromHexSeed(res.key);
-            }
-            successCallback(res);
-        }, errorCallback, "TouchID", "initKey", [params.keyId, params.message]);
+var ua = navigator.userAgent;
+var checker = {
+    iphone: ua.match(/(iPhone|iPod|iPad)/),
+    blackberry: ua.match(/BlackBerry/),
+    android: ua.match(/Android/)
 };
 
-FingerprintKey.prototype.fetchKey = function(params, successCallback, errorCallback) {
-    cordova.exec(
-        function(res) {
-            if (res.status == "ok") {
-                res.key = createKeyFromHexSeed(res.key);
-            }
-            successCallback(res);
-        }, errorCallback, "TouchID", "fetchKey", [params.keyId, params.message]);
-};
+if (checker.iphone) {
+    FingerprintKey.prototype.getDevice = function() {
+        return "iOS";
+    }
+    FingerprintKey.prototype.initKey = function(params, successCallback, errorCallback) {
+        cordova.exec(
+            function(res) {
+                if (res.status == "ok") {
+                    res.key = createKeyFromHexSeed(res.key);
+                }
+                successCallback(res);
+            }, errorCallback, "TouchID", "initKey", [params.keyId, params.message]);
+    };
 
-FingerprintKey.prototype.isAvailable = function(successCallback, errorCallback) {
-    cordova.exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
-};
+    FingerprintKey.prototype.fetchKey = function(params, successCallback, errorCallback) {
+        cordova.exec(
+            function(res) {
+                if (res.status == "ok") {
+                    res.key = createKeyFromHexSeed(res.key);
+                }
+                successCallback(res);
+            }, errorCallback, "TouchID", "fetchKey", [params.keyId, params.message]);
+    };
+
+    FingerprintKey.prototype.isAvailable = function(successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
+    };
+} else if (checker.android) {
+    FingerprintKey.prototype.getDevice = function() {
+        return "Android";
+    }
+    FingerprintKey.prototype.initKey = function(params, successCallback, errorCallback) {
+        cordova.exec(
+            function(res) {
+                if (res.status == "ok") {
+                    res.key = createKeyFromHexSeed(res.key);
+                }
+                successCallback(res);
+            },
+            errorCallback,
+            "FingerprintKey", // Java Class
+            "initkey", // action
+            [ // Array of arguments to pass to the Java class
+                params
+            ]
+        );
+    };
+
+    FingerprintKey.prototype.fetchKey = function(params, successCallback, errorCallback) {
+        cordova.exec(
+            function(res) {
+                if (res.status == "ok") {
+                    res.key = createKeyFromHexSeed(res.key);
+                }
+                successCallback(res);
+            },
+            errorCallback,
+            "FingerprintKey", // Java Class
+            "fetchkey", // action
+            [ // Array of arguments to pass to the Java class
+                params
+            ]
+        );
+    };
+
+    FingerprintKey.prototype.isAvailable = function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback,
+            errorCallback,
+            "FingerprintKey", // Java Class
+            "availability", // action
+            [{}]
+        );
+    };
+}
 
 FingerprintKey = new FingerprintKey();
 window.FingerprintKey = FingerprintKey;
